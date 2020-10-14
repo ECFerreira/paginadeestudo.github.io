4153aaea98bb7d5840ce82f777555e9782e3ff6f
ECFerreira <eduardocamilo.seas@gmail.com>
2020-10-14 11:15:23 -0300

    14/10/20_2

---
+++ teste.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Conteúdo do cabeçalho-->
+        <title>Mirror Fashion</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/estilo.css">
+        <link rel="stylesheet" href="css/mobile.css" media="max-width: 939px">
+        <meta name="viewport" content="width=device-width">
+
+        <?php
+    // código PHP aqui
+    ?>
+
+    <h1> HTML aqui </h1>
+    <?php
+        // código PHP aqui
+        ?>
+    <p> Mais HTML</p>
+
+    </head>
+
+
+    <body>
+ 
+     </body>
+</html>
+    
+   
